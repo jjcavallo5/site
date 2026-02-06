34503f075d0759db132956ab0f285e83a0eca963
Jeremy Cavallo <jcbball55@gmail.com>
2026-02-06 07:28:27 -0500

    Some tailwind var setup and remove boilerplate

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: [
+          "FiraCode Nerd Font",
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "monospace",
+        ],
+      },
+    },
+  },
+};
